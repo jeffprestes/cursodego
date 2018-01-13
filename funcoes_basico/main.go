fb927446e65f19d1b66738559fce7ffb239fe55c
Jeff Prestes <jeffprestes@gmail.com>
2018-01-13 12:39:38 -0200

    Adicionando soma no exemplo e ajustando nome dos pacotes

--- funcoes_basico/main.go
+++ funcoes_basico/main.go
@@ -9,6 +9,9 @@ func main() {
 	fmt.Printf("O resultado da soma foi: %d\r\n", resultado)
 
 	resultado = matematica.Calculo(matematica.Multiplicador, 2, 2)
-	fmt.Printf("O resultado da multiplicacao foi: %d\r\n", resultado)
-	
+	fmt.Printf("O resultado da calculo foi: %d\r\n", resultado)
+
+	resultado = matematica.Calculo(matematica.Soma, 5, 4)
+	fmt.Printf("O resultado do calculo foi: %d\r\n", resultado)
+
 }
