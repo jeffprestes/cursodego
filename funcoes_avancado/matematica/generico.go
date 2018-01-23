c2a80603fd3317b4bd21ea0a81570ff13f2dabc9
Jeff Prestes <jeffprestes@gmail.com>
2018-01-23 13:56:02 -0200

    Melhorias de exemplos

--- funcoes_avancado/matematica/generico.go
+++ funcoes_avancado/matematica/generico.go
@@ -3,7 +3,7 @@ package matematica
 /*
 Calculo executa qualquer tipo de calculo basta enviar a funcao desejada
 */
-func Calculo(funcao func(int, int) int, numeroA int, numeroB int) int {
+func Calculo(funcao fnCalculo, numeroA int, numeroB int) int {
 	return funcao(numeroA, numeroB)
 }
 
---
+++ funcoes_avancado/matematica/tipo.go
@@ -0,0 +1,5 @@
+package matematica
+
+//fnCalculo tipo de funcao que realiza um calculo com 2 parametros
+//e retorna um resultado
+type fnCalculo func(int, int) int
