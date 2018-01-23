a9bc44829db8d06d7c42eddeec68267ca02cad26
Jeff Prestes <jeffprestes@gmail.com>
2018-01-23 12:41:41 -0200

    Adiciona nova funcao

--- structs_avancado/model/imovel.go
+++ structs_avancado/model/imovel.go
@@ -13,6 +13,13 @@ func (i *Imovel) SetValor(valor int) {
 	i.valor = valor
 }
 
+//DobraValor dobra o valor informado
+func (i *Imovel) DobraValor(valor int) (resultado int) {
+	i.valor = valor
+	resultado = i.valor * 2
+	return
+}
+
 //GetValor retorna o valor do Imovel
 func (i *Imovel) GetValor() int {
 	return i.valor
